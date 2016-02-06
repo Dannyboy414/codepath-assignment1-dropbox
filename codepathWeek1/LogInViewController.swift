08194a430b01509d5dfee0a7caf56dc77070868f
Dannyboy414 <dannyboy414@gmail.com>
2016-02-05 22:07:13 -0800

    Sign in page with uiTextField.

---
+++ codepathWeek1/LogInViewController.swift
@@ -0,0 +1,50 @@
+//
+//  LogInViewController.swift
+//  codepathWeek1
+//
+//  Created by Daniel Kim on 2/5/16.
+//  Copyright © 2016 Daniel Kim. All rights reserved.
+//
+
+import UIKit
+
+class LogInViewController: UIViewController, UITextFieldDelegate {
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        emailTextField.becomeFirstResponder()
+        // Do any additional setup after loading the view.
+        
+        self.emailTextField.delegate = self
+        self.passwordTextField.delegate = self
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        if self.passwordTextField.text?.characters.count >= 8 && self.emailTextField.text?.characters.count > 0 {
+            self.loginButton.enabled = true
+        } else {
+            self.loginButton.enabled = false
+        }
+        return true
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
